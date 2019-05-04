4fd966935671f6bc3a50b391356014c88cc5bbd9
Tyler Weeres <tweeres04@gmail.com>
2019-05-04 11:47:23 -0700

    Added bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-import './App.css';
+import 'bulma/css/bulma.css';
 
 export default function App() {
-	return <p>This is Better Retros</p>;
+	return (
+		<section className="section">
+			<div className="container">
+				<p>This is Better Retros</p>
+			</div>
+		</section>
+	);
 }
