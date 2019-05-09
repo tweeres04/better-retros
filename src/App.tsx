3500121af1e31750480c4d731d406b35601e83d2
Tyler Weeres <tweeres04@gmail.com>
2019-05-08 23:21:34 -0700

    Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -36,87 +36,87 @@ export default function App({
 
 	return (
 		<>
-			<section className="section">
-				<div className="navbar is-spaced">
-					<div className="container">
-						<div className="navbar-brand">
+			<nav className="navbar is-spaced">
+				<div className="container">
+					<div className="navbar-brand">
+						<div className="navbar-item">
 							<h1 className="title is-4">Better Retros</h1>
 						</div>
 					</div>
 				</div>
+			</nav>
 
-				<div className="container">
-					<div className="level is-mobile">
-						<div className="level-left">
-							<div className="level-item">
-								<div className="field">
-									<label htmlFor="name" className="label">
-										Your name
-									</label>
-									<div className="control">
-										<input
-											type="text"
-											id="name"
-											className="input"
-											onChange={(e): void => {
-												setName(e.target.value);
-											}}
-											value={name}
-										/>
-									</div>
+			<div className="container">
+				<div className="level is-mobile">
+					<div className="level-left">
+						<div className="level-item">
+							<div className="field">
+								<label htmlFor="name" className="label">
+									Your name
+								</label>
+								<div className="control">
+									<input
+										type="text"
+										id="name"
+										className="input"
+										onChange={(e): void => {
+											setName(e.target.value);
+										}}
+										value={name}
+									/>
 								</div>
 							</div>
-							<div className="level-item">
-								<div className="field">
-									<label htmlFor="retroId" className="label">
-										Retro ID
-									</label>
-									<div className="control">
-										<input
-											type="text"
-											id="retroId"
-											className="input"
-											onKeyDown={(e): void => {
-												if (e.key == 'Enter') {
-													handleRetroIdSubmit(e);
-												}
-											}}
-											onChange={(e): void => {
-												setRetroId(e.target.value);
-											}}
-											onBlur={handleRetroIdSubmit}
-											value={retroId}
-										/>
-									</div>
+						</div>
+						<div className="level-item">
+							<div className="field">
+								<label htmlFor="retroId" className="label">
+									Retro ID
+								</label>
+								<div className="control">
+									<input
+										type="text"
+										id="retroId"
+										className="input"
+										onKeyDown={(e): void => {
+											if (e.key == 'Enter') {
+												handleRetroIdSubmit(e);
+											}
+										}}
+										onChange={(e): void => {
+											setRetroId(e.target.value);
+										}}
+										onBlur={handleRetroIdSubmit}
+										value={retroId}
+									/>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className="columns is-mobile">
-						{!loading && !loadedRetroId ? (
-							<div className="column">
-								<div className="card">
-									<div className="card-content has-text-centered">
-										No Retro loaded. Enter a Retro ID to get started!
-									</div>
+				</div>
+				<div className="columns is-mobile">
+					{!loading && !loadedRetroId ? (
+						<div className="column">
+							<div className="card">
+								<div className="card-content has-text-centered">
+									No Retro loaded. Enter a Retro ID to get started!
 								</div>
 							</div>
-						) : loading ? null : (
-							columns.map(
-								({ header, items, addItem }): JSX.Element => (
-									<Column
-										key={header}
-										header={header}
-										items={items}
-										name={name}
-										addItem={addItem}
-									/>
-								),
-							)
-						)}
-					</div>
+						</div>
+					) : loading ? null : (
+						columns.map(
+							({ header, items, addItem }): JSX.Element => (
+								<Column
+									key={header}
+									header={header}
+									items={items}
+									name={name}
+									addItem={addItem}
+								/>
+							),
+						)
+					)}
 				</div>
-			</section>
+			</div>
 		</>
 	);
 }
